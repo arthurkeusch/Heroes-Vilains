2e56974f3a8f7e88148e1e4cc69445fd413ea851
Arthur KEUSCH <arthur.keusch@edu.univ-fcomte.fr>
2024-01-23 14:59:07 +0100

    Services hero et axios

--- heroes-vilains/src/services/axios.service.js
+++ heroes-vilains/src/services/axios.service.js
@@ -7,30 +7,13 @@ const axiosAgent = axios.create({
 function handleError(serviceName, err) {
     if (err.response) {
         console.log("ERROR while calling SERVICE " + serviceName + ": " + JSON.stringify(err.response));
-        return {
-            data: {
-                error: 1,
-                data: err.response.data
-            }
-        };
-    }
-    else if (err.request) {
+        return {data: {error: 1, data: err.response.data}};
+    } else if (err.request) {
         console.log("NETWORK ERROR while calling SERVICE "+serviceName+ ": " + JSON.stringify(err.request));
-        return {
-            data: {
-                error: 1,
-                data: 'Le serveur est injoignable ou l\'URL demandée n\'existe pas'
-            }
-        };
-    }
-    else {
+        return {data: {error: 1, data: 'Le serveur est injoignable ou l\'URL demandée n\'existe pas'}};
+    } else {
         console.log("UNKNOWN ERROR while calling SERVICE "+serviceName);
-        return {
-            data: {
-                error: 1,
-                data: 'Erreur inconnue'
-            }
-        };
+        return {data: {error: 1, data: 'Erreur inconnue'}};
     }
 }
 async function getRequest(uri, name) {
@@ -75,9 +58,20 @@ async function deleteRequest(uri, data, name) {
     return response.data;
 }
 
+async function putRequest(uri, data, name) {
+    let response = null
+    try {
+        response = await axiosAgent.put(uri, data)
+    } catch (err) {
+        response = handleError(name, err);
+    }
+    return response.data;
+}
+
 export {
     getRequest,
     postRequest,
     deleteRequest,
-    patchRequest
+    patchRequest,
+    putRequest
 }
--- heroes-vilains/src/services/hero.service.js
+++ heroes-vilains/src/services/hero.service.js
@@ -1,46 +1,11 @@
-import {getRequest,postRequest,deleteRequest,patchRequest} from "./axios.services";
+import {getRequest, postRequest, putRequest} from "./axios.service";
 
-async function getAllConcertFromAPI() {
-    return getRequest('/artistes/getAllConcert', 'getAllConcert');
+async function getAliasesFromAPI() {
+    return getRequest('/heroes/getaliases', 'getAliases');
 }
 
-async function getAllConcert() {
-    let answer = await getAllConcertFromAPI();
-    return answer.data;
-}
-
-
-
-async function getAllTypeMusiqueFromAPI() {
-    return getRequest('/artistes/getAllTypeMusique', 'getAllTypeMusique');
-}
-
-async function getAllTypeMusique() {
-    let answer = await getAllTypeMusiqueFromAPI();
-    return answer.data;
-}
-
-
-
-
-async function getAllScenesFromAPI() {
-    return getRequest('/artistes/getAllScenes', 'getAllScenes');
-}
-
-async function getAllScenes() {
-    let answer = await getAllScenesFromAPI();
-    return answer.data;
-}
-
-
-
-
-async function getInfoGroupeFromAPI(id_groupe) {
-    return getRequest('/artistes/concert/getInfoGroupe?id='+id_groupe, 'getInfoGroupe');
-}
-
-async function getInfoGroupe(id_groupe) {
-    let answer = await getInfoGroupeFromAPI(id_groupe);
+async function getAliases() {
+    let answer = await getAliasesFromAPI();
     return answer.data;
 }
 
@@ -48,127 +13,55 @@ async function getInfoGroupe(id_groupe) {
 
 
 
-
-async function getInfoArtistesGroupeFromAPI(id_groupe) {
-    return getRequest('/artistes/concert/getInfoArtistesGroupe?id='+id_groupe, getInfoArtistesGroupe);
+async function createHeroFromAPI(data) {
+    return postRequest('/heroes/create',data, 'createHero')
 }
 
-async function getInfoArtistesGroupe(id_groupe) {
-    let answer = await getInfoArtistesGroupeFromAPI(id_groupe);
-    return answer.data;
-}
-
-
-
-
-
-async function getInfoConcertFromAPI(id_concert) {
-    return getRequest('/artistes/concert/getInfoConcert?id='+id_concert, getInfoConcert);
-}
-
-async function getInfoConcert(id_concert) {
-    let answer = await getInfoConcertFromAPI(id_concert);
-    return answer.data;
-}
-
-
-
-
-
-async function getAllConcertWithFilterFromAPI(data) {
-    const params = new URLSearchParams(data);
-    return getRequest(`/artistes/getAllConcertWithFilter?${params}`);
-}
-
-
-async function getAllConcertWithFilter(data) {
-    let answer = await getAllConcertWithFilterFromAPI(data);
-    console.log(data);
-    return answer.data;
-}
-
-async function getAllArtistesFromAPI() {
-    return getRequest('/artistes/getAllArtistes');
-}
-
-
-async function getAllArtistes() {
-    let answer = await getAllArtistesFromAPI();
-    return answer.data;
-}
-
-async function getCommentairesFromAPI(id_concert) {
-    return getRequest('/artistes/concert/getCommentaires?id='+id_concert, "getCommentaires");
-}
-
-async function getCommentaires(id_concert) {
-    let answer = await getCommentairesFromAPI(id_concert);
-    return answer.data;
-}
-
-async function saveCommentFromAPI(data) {
-    return postRequest('/artistes/concert/saveCommentaire',data, 'createConcert')
-}
-
-async function saveComment(id,prenom,nom,texte,id_stand) {
+async function createHero(publicName, realName, powers) {
     let data = {
-        id_utilisateur : id,
-        prenom_utilisateur : prenom,
-        nom_utilisateur: nom,
-        texte_commentaire : texte,
-        id_stand : id_stand,
-    }
-    let answer = await saveCommentFromAPI(data)
-    return answer
+        publicName : publicName,
+        realName : realName
+    };
+    if (powers !== null) data.powers = powers;
+    let answer = await createHeroFromAPI(data)
+    return answer.data
 }
 
-async function deleteCommentaireFromAPI(id) {
-    return deleteRequest('/artistes/concert/deleteCommentaire?id='+id, 'createConcert')
+
+
+
+async function updateHeroFromAPI(data, orgSecret) {
+    return putRequest('/heroes/update?org-secret='+orgSecret,data, 'updateHero')
 }
 
-async function deleteCommentaire(id) {
-    let answer = await deleteCommentaireFromAPI(id)
-    return answer
-}
-
-async function modifierCommentaireFromAPI(data) {
-    return patchRequest('/artistes/concert/modifierCommentaire',data, 'createConcert')
-}
-
-async function modifierCommentaire(id,texte) {
+async function updateHero(id, publicName, realName, powers, orgSecret) {
     let data = {
-        id :id,
-        texte :texte
-    }
-    let answer = await modifierCommentaireFromAPI(data)
-    return answer
+        _id: id
+    };
+    if (publicName !== null) data.publicName = publicName;
+    if (realName !== null) data.realName = realName;
+    if (powers !== null) data.powers = powers;
+    let answer = await updateHeroFromAPI(data, orgSecret)
+    return answer.data
 }
 
-async function addArtisteToGroupeFromAPI(data) {
-    return patchRequest('/artistes/addArtisteToGroupe',data, 'createConcert')
+
+
+async function getHeroByIDFromAPI(id, orgSecret) {
+    return getRequest('/heroes/getbyid/' + id + '?org-secret=' + orgSecret, 'getHeroByID');
 }
 
-async function addArtisteToGroupe(id_artiste,id_groupe) {
-    let data = {
-        id_artiste :id_artiste,
-        id_groupe :id_groupe
-    }
-    let answer = await addArtisteToGroupeFromAPI(data)
-    return answer
+async function getHeroByID(id, orgSecret) {
+    let answer = await getHeroByIDFromAPI(id, orgSecret);
+    return answer.data;
 }
+
+
+
 
 export {
-    getAllConcert,
-    getAllTypeMusique,
-    getAllScenes,
-    getInfoGroupe,
-    getInfoArtistesGroupe,
-    getInfoConcert,
-    getAllConcertWithFilter,
-    getCommentaires,
-    saveComment,
-    deleteCommentaire,
-    modifierCommentaire,
-    getAllArtistes,
-    addArtisteToGroupe
+    getAliases,
+    createHero,
+    updateHero,
+    getHeroByID
 }
