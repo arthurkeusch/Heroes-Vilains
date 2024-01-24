e64a18eab3ea2a61246a88e40090cc370004c75c
Arthur KEUSCH <arthur.keusch@edu.univ-fcomte.fr>
2024-01-24 09:45:43 +0100

    Création du store

--- heroes-vilains/src/store/index.js
+++ heroes-vilains/src/store/index.js
@@ -4,14 +4,47 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        passwordOrganisation: null,
+        currentHero: null,
+        currentTeam: null,
+        currentOrganisation: null,
+        listHeroAlias: [],
+        listTeam: [],
+        listOrganisations: []
+    },
+
+    mutations: {
+        updatePasswordOrganisation(state, password) {
+            state.passwordOrganisation = password;
+        },
+
+        updateListHeroAlias(state, listHero) {
+            state.listHeroAlias = listHero;
+        },
+
+        updateCurrentHero(state, currentHero) {
+            state.currentHero = currentHero;
+        },
+
+        updateListTeam(state, listTeam) {
+            state.listTeam = listTeam;
+        },
+
+        updateCurrentTeam(state, currentTeam) {
+            state.currentTeam = currentTeam;
+        },
+
+        updateListOrganisations(state, listOrganisations) {
+            state.listOrganisations = listOrganisations;
+        },
+
+        updateCurrentOrganisation(state, currentOrganisation) {
+            state.currentOrganisation = currentOrganisation;
+        },
+    },
+
+    actions: {
+
+    },
 })
