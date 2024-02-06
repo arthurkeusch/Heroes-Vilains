87e31b6cc1fe4593c8e5991c1c2b054ab51e8f12
Lysdoryah <cecile.kuzniack@edu.univ-fcomte.fr>
2024-02-06 14:25:38 +0100

    navbar

--- heroes-vilains/src/App.vue
+++ heroes-vilains/src/App.vue
@@ -1,55 +1,70 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app class="mx-auto overflow-hidden" >
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
 
-      <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
 
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+        <v-app-bar
+                height="70"
+                dark
+                prominent
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+
+
+
+        </v-app-bar>
+
+        <v-navigation-drawer
+                v-model="drawer"
+                temporary
+        >
+            <v-list
+
+                    nav
+                    dense
+            >
+                <v-list-item-group
+                        v-model="group"
+
+
+                >
+                    <v-list-item>
+                        <v-list-item-title>Foo</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-title>Bar</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-title>Fizz</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-title>Buzz</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-spacer></v-spacer>
+
+    </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+    data: () => ({
+        drawer: false,
+        group: null,
+    }),
 
-  data: () => ({
-    //
-  }),
-};
+    watch: {
+        group () {
+            this.drawer = false
+        },
+    },
+}
 </script>
