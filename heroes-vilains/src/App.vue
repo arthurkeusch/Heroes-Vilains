9050199ab2ba222d1bb823264e3897232926b12d
Arthur KEUSCH <arthur.keusch@edu.univ-fcomte.fr>
2024-02-07 10:40:19 +0100

    Mise à jour NavBar

--- heroes-vilains/src/App.vue
+++ heroes-vilains/src/App.vue
@@ -1,81 +1,74 @@
 <template>
-    <v-app class="mx-auto overflow-hidden" >
+  <v-app class="mx-auto overflow-hidden">
 
+    <v-main>
+      <v-app-bar
+          height="70"
+          dark
+          prominent>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="redirect(4)">
+          <h3>Login</h3>
+        </v-btn>
+      </v-app-bar>
 
-        <v-main>
-
-        <v-app-bar
-                height="70"
-                dark
-                prominent
-        >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="redirect(1)">
-                <h3>Login</h3>
-            </v-btn>
-
-        </v-app-bar>
-
-<div style="display: flex">
-
-
+      <div style="display: flex">
         <router-view style="order: 2"/>
         <v-navigation-drawer
-                v-model="drawer"
-                style="order: 1"
-        >
-            <v-list
+            v-model="drawer"
+            style="order: 1">
+          <v-list>
+            <v-list-item-group v-model="group">
+              <v-list-item>
+                <v-list-item-title @click="redirect(1)">Accueil</v-list-item-title>
+              </v-list-item>
 
-            >
-                <v-list-item-group
-                        v-model="group"
+              <v-list-item>
+                <v-list-item-title @click="redirect(2)">Organisations</v-list-item-title>
+              </v-list-item>
 
-
-                >
-                    <v-list-item>
-                        <v-list-item-title @click="redirect(1)">Login</v-list-item-title>
-                    </v-list-item>
-
-                    <v-list-item>
-                        <v-list-item-title @click="redirect(2)">Organisations</v-list-item-title>
-                    </v-list-item>
-
-                    <v-list-item>
-                        <v-list-item-title @click="redirect(3)">Equipes</v-list-item-title>
-                    </v-list-item>
-
-                </v-list-item-group>
-            </v-list>
-
+              <v-list-item>
+                <v-list-item-title @click="redirect(3)">Equipes</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
         </v-navigation-drawer>
-</div>
-          </v-main>
+      </div>
+    </v-main>
 
-    </v-app>
+  </v-app>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        drawer: false,
-        group: null,
-    }),
+  name: 'App',
 
-    watch: {
-        group () {
-            this.drawer = false
-        },
-    },
-    methods: {
-        async redirect(id){
-            switch (id){
-                case 1 : await this.$router.push('/login'); break;
-                case 2 : await this.$router.push('/organisation'); break;
-                case 3 : await this.$router.push('/team/list'); break;
-            }
-
-        }
+  data() {
+    return {
+      drawer: false,
+      group: null
     }
+  },
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+
+  methods: {
+    async redirect(id) {
+      if (id === 1 && this.$route.path !== '/') {
+        await this.$router.push('/');
+      } else if (id === 2 && this.$route.path !== '/organisation') {
+        await this.$router.push('/organisation');
+      } else if (id === 3 && this.$route.path !== '/team/list') {
+        await this.$router.push('/team/list');
+      } else if (id === 4 && this.$route.path !== '/login') {
+        await this.$router.push('/login');
+      }
+    }
+  }
 }
 </script>
